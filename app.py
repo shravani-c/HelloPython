2377f8249f9eb5870fffc254e9cbb5318cd6404b
shravani <shravani.chanu@gmail.com>
2016-06-02 09:53:55 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import tornado.ioloop
+import tornado.web
+import os
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello World")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    port = os.environ['PORT'] if os.environ['PORT'] else 8888
+    application.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
